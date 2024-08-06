d6248db81a2a47c4acef4b8a75e3deb4e3638714
Hepsiba Sona Jo <hepsijo18@gmail.com>
2024-08-06 14:24:24 +0100

    Upadated with components

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles.css';
+import Header from './components/Header'; 
+import Footer from './components/Footer';
+import MoviesGrid from './components/MoviesGrid';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className='container'>
+        <Header></Header>
+        <MoviesGrid></MoviesGrid>
+      </div>
+
+
+
+
+      <div>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles.css';
+
+
+
+export default function Header(){
+
+return(
+    <>
+        <div className="header">
+        <img className="logo" src="logo.png" alt="Moviedux" />
+
+        </div>
+    </>
+)
+
+}
+
+
